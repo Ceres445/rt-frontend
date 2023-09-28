6bc5bc33f5617fed43b89228e428d25d956ed4b4
VedaantAchuthan <xyrunthroughs@gmail.com>
2023-09-28 22:24:18 +0530

    added more features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+//import * as altaz from './RaDecAltAz.js';
+
+let coordinates = [
+  { id: 0, type: 'ra' },
+  { id: 1, type: 'dec' }
+];
+
+function MyForm() {
+  const [coord, setCoord] = useState(coordinates)
+  const [input, setInput] = useState("");
+
+  const updateState = (index) => (e) => {
+    const newArray = coord.map((item, i) => {
+      if (index === i) {
+        return { ...item, [e.target.name]: e.target.value };
+      } else {
+        return item;
+      }
+    });
+    setCoord(newArray);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {coord.map((datum, index) => {
+        <li key={datum.name}>
+          <input
+            type="text"
+            name="name"
+            value={datum.name}
+            onChange={updateState(index)}
+          />
+        </li>;
+        })}
+      <form onSubmit={(event) => event.preventDefault()}>
+        <label>Enter Right Ascention<br />
+          <input
+            type="text"
+            name="ra"
+            onChange={saveInput}
+          />
+        </label>
+        <br />
+        <label>Enter Declination<br />
+          <input
+            type="text"
+            name="dec"
+            onChange={saveInput}
+          />
+        </label>
+        <br />
+        <button type='Submit' onClick={updateState()}
+        >Submit</button>
+      </form>
+
+    </>
+  )
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app!</h1>
+      <MyForm />
     </div>
   );
 }
-
-export default App;
